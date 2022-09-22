40c451d1f9a0ec790e81b3c6910c8ccb20c53a3e
Pham Ngoc Long <94893379+LongPN-BE@users.noreply.github.com>
2022-09-22 11:05:10 +0700

    deploy swagger

--- src/main/java/com/groupswd391/fall22/Fall22Application.java
+++ src/main/java/com/groupswd391/fall22/Fall22Application.java
@@ -2,8 +2,11 @@ package com.groupswd391.fall22;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
 
 @SpringBootApplication
+@EnableSwagger2
 public class Fall22Application {
 
 	public static void main(String[] args) {
