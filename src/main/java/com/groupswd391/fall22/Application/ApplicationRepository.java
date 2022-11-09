a38a6eb598f146e878eeac25ebb4dac65191279a
Pham Ngoc Long <94893379+LongPN-BE@users.noreply.github.com>
2022-11-09 19:03:58 +0700

    Fix foreign key

--- src/main/java/com/groupswd391/fall22/Application/ApplicationRepository.java
+++ src/main/java/com/groupswd391/fall22/Application/ApplicationRepository.java
@@ -11,7 +11,7 @@ public interface ApplicationRepository extends JpaRepository<Application, Intege
     @Query(value = "SELECT * from application a left join user u on u.id = a.user_ID where u.id = ?", nativeQuery = true)
     Page<Application> findByUserID(int id, Pageable pagingSort);
 
-    @Query(value = "SELECT * from application a left join project_item pi on pi.id = a.projectItem_ID where pi.id = ?", nativeQuery = true)
+    @Query(value = "SELECT * from application a left join project_item pi on pi.id = a.project_item_id where pi.id = ?", nativeQuery = true)
     Page<Application> findByProjectItem(int id, Pageable pagingSort);
 
 
